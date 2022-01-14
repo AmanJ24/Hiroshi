19a0a488c41e9fba40fc868a7a91a3fd6e762c34
AmanJ <amanjangir947@gmail.com>
2022-01-14 23:23:33 +0530

    Whatever

--- Py/Turtle OOP.py
+++ Py/Turtle OOP.py
@@ -12,3 +12,13 @@
 # print(my_screen.canvheight)
 # my_screen.exitonclick()
 
+from prettytable import PrettyTable
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = "l"
+table.header_style = "title"
+print(table)
+
