101ae7b18b315943efcb3619f86c37f932ddeecf
ManveerSingh <manveersingh@pop-os.localdomain>
2022-01-29 13:52:22 +0530

    nothing

--- c++/courseThings/somethingNew.cpp
+++ c++/courseThings/somethingNew.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
+
 using namespace std;
-
-
 class pro{
     public:
 
@@ -13,34 +12,20 @@ class pro{
     }
 
     pro operator+(const pro& other){
-        this->something += other.something;
-        this->xp += other.xp;
+        this->something = other.something;
+        this->xp = other.xp;
         return *this;
-    }
-
-    void operator-(void){
-        this->something = 100;
-        this->xp = 200;
-        cout << this->xp << endl;
-        cout << this->something << endl;
-    }
-
-
-    bool operator==(const pro& other){
-        if(this->something == other.something)return true;
-        else
-            return false;
-        
     }
     void talk(){
         cout << "HELLO manveer" << endl;
     }
 };
+
 int main(){
     pro manveer;
     pro sakshi;
 
-    manveer.something = 100;
-    sakshi.something = 100;
+    manveer.something = 1024;
 
+    sakshi = sakshi.operator+(manveer);
 }
