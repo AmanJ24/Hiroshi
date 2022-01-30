09a9b7f92c56a57d102e5aa0324a6095cd3c53b8
ManveerSingh <manveersingh@pop-os.localdomain>
2022-01-30 19:36:16 +0530

    nothing

--- c++/courseThings/somethingNew.cpp
+++ c++/courseThings/somethingNew.cpp
@@ -1,39 +1,15 @@
 #include <iostream>
 
 using namespace std;
+
 class pro{
+    private:
+    int something;
+    int health;
     public:
-    int something = 1;
-    int xp = 1;
 
-    friend bool operator==(pro& other, pro& other1){
-        cout << "Something good is going to happen!" << endl;
-        return true;
-    }
-    pro(){
-
-    }
-
-    bool operator+(const pro& other){
-        this->something = other.something;
-        this->xp = other.xp;
-        return true;
-    }
-    // bool operator==(const pro& other){
-        // if(this->something == other.something)return 1;
-        // else{
-            // return 0;
-        // }
-    // }
-    void talk(){
-        cout << "HELLO manveer" << endl;
-    }
 };
-
 int main(){
-    pro manveer;
-
-    pro sakshi;
-
-    manveer == sakshi;
-}   
+    cout << "Hello World" << endl;
+    return 0;
+}
