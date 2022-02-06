ed82acb4f951c2f0e9d1f1e5fedaa59b9e687701
ManveerSingh0 <singhcoder2005@gmail.com>
2022-02-06 17:57:02 +0530

    Now i will learn to use sfml to make 3d games

--- SFML/SFML learning/main.cpp
+++ SFML/SFML learning/main.cpp
@@ -1,6 +1,10 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
+#include "src/include/SFML/hello.h"
 int main()
 {
+    pro manveer;
+    manveer.talk();
     sf::RenderWindow window(sf::VideoMode(600,600), "ManveerSingh");
     return 0;
 }
