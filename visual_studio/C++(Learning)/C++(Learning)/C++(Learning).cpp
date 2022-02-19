92d6806774cb6c0e4209f261dcbcd8d001f6f1a9
ManveerSingh0 <singhcoder2005@gmail.com>
2022-02-19 19:20:40 +0530

    nothing

--- visual_studio/C++(Learning)/C++(Learning)/C++(Learning).cpp
+++ visual_studio/C++(Learning)/C++(Learning)/C++(Learning).cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 
-using namespace std;
 
 
 int main()
 {
-	int num{ 100 };
-	cout << dec << num << endl;
-	cout << hex << num << endl;
-	cout << oct << num << endl;
+	std::cout << "Hello, World" << std::endl;
+	std::cin.get();
 	return 0;
 }
 
