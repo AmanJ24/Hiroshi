655e9c0c94f9e006452cfb9509013d547acfe74a
AmanJ <amanjangir947@gmail.com>
2022-02-27 17:22:27 +0530

    GUI PROGRAMS

---
+++ Py/Other Files/list comprehension.py
@@ -0,0 +1,11 @@
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+# import random 
+
+# scores = {student: random.randint(1, 100) for student in names}
+
+# passed = {student:score for (student, score) in scores if score >= 60}
+
+
+value = input("Enter the string: ")
+stipped = list(value)
+
--- Py/US states guessing game/main.py
+++ Py/US states guessing game/main.py
@@ -1,25 +1,40 @@
 import turtle
 import pandas
 
-writer = turtle.Turtle()
-writer.penup()
-writer.hideturtle()
 screen = turtle.Screen()
 screen.title("US States Game")
 
 image = "blank_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+screen.screensize(200, 200)
+
+writer = turtle.Turtle()
+writer.penup()
+writer.hideturtle()
 
 data = pandas.read_csv("50_states.csv")
 
-answer_state = screen.textinput("Guess the State","What's another state's name")
-print(answer_state)
+is_game_on = True
+Correct_count = 0
 
-for line in data:
-    if answer_state in line:
-        print("its here")
+while is_game_on:
+    answer = screen.textinput(f"{Correct_count}/50 States Guessed","What's another state's name")
+    answer_state = answer.capitalize()
 
-
+    if Correct_count < 50:
+        for line in data.state:
+            if answer_state in line:
+                Correct_count += 1
+                x_cor = int(data[data.state == answer_state].x)
+                y_cor = int(data[data.state == answer_state].y)
+                writer.goto(x_cor,y_cor)
+                writer.write(answer_state)
+            else:
+                pass    
+    else:
+        is_game_on = False
+        writer.goto(0,0)
+        writer.write("You Win!!!", font = ["Arial", 20, "normal"])        
 
 screen.exitonclick()
