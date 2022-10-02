6099a496cb9e80350caf3d71860f7d31589cd6d0
AmanJ <amanjangir947@gmail.com>
2022-10-02 21:44:59 +0530

    Locate

--- Py/Other Files/tempCodeRunnerFile.py
+++ Py/Other Files/tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-plt.pie(num)
+def first_non_repeating_letter(string):
+    answer = [i for i in string if string.lower().count(i) == 1]
+    return '' if answer == [] else answer
+
+
+first_non_repeating_letter("sTreSS")    
